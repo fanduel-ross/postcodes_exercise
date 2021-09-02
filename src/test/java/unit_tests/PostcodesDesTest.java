bbe2b09479c067e7b380922ce5643ac6acaecdbe
Ross Blair <ross.blair@fanduel.com>
2021-09-02 09:54:32 +0100

    Adding more Unit Tests

--- src/test/java/unit_tests/PostcodesDesTest.java
+++ src/test/java/unit_tests/PostcodesDesTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -72,6 +76,101 @@ public class PostcodesDesTest {
         assertEquals( -4.254854, postcodeDto.getLongitude());
     }
 
+    @Test
+    public void getLatitudeTest(){
+        assertEquals( 55.891307, postcodeDto.getLatitude());
+    }
+
+    @Test
+    public void getElectoralRegionTest(){
+        assertEquals( "Scotland", postcodeDto.getEuropeanElectoralRegion());
+    }
+
+    @Test
+    public void getPrimaryCareTrustTest(){
+        assertEquals( "Glasgow City Community Health Partnership",
+                postcodeDto.getPrimaryCareTrust());
+    }
+
+    @Test
+    public void getLsoaTest(){
+        assertEquals( "Possil Park - 03",
+                postcodeDto.getLsoa());
+    }
+
+    @Test
+    public void getMsoaTest(){
+        assertEquals( "Possil Park",
+                postcodeDto.getMsoa());
+    }
+
+    @Test
+    public void getIncodeTest(){
+        assertEquals( "6LS",
+                postcodeDto.getIncode());
+    }
+
+    @Test
+    public void getOutcodeTest(){
+        assertEquals( "G22",
+                postcodeDto.getOutcode());
+    }
+
+    @Test
+    public void getParliamentaryConstituencyTest(){
+        assertEquals( "Glasgow North East",
+                postcodeDto.getParliamentaryConstituency());
+    }
+
+
+    @Test
+    public void getAdminDistrictTest(){
+        assertEquals( "Glasgow City",
+                postcodeDto.getAdminDistrict());
+    }
+
+    @Test
+    public void getAdminWardTest(){
+        assertEquals( "Canal",
+                postcodeDto.getAdminWard());
+    }
+
+    @Test
+    public void getCcgTest(){
+        assertEquals( "Glasgow City Community Health Partnership",
+                postcodeDto.getCcg());
+    }
+
+    @Test
+    public void getNutsTest(){
+        assertEquals( "Glasgow City",
+                postcodeDto.getNuts());
+    }
+
+    @Test
+    public void getCodes(){
+        HashMap<String, String> codesMap = new HashMap<>();
+
+        codesMap.put("admin_district", "S12000049");
+        codesMap.put("admin_county", "S99999999");
+        codesMap.put("admin_ward", "S13002982");
+        codesMap.put("parish", "S99999999");
+        codesMap.put("parliamentary_constituency", "S14000032");
+        codesMap.put("ccg", "S03000043");
+        codesMap.put("ccg_id", "043");
+        codesMap.put("ced", "S99999999");
+        codesMap.put("nuts", "TLM82");
+        codesMap.put("lsoa", "S01010325");
+        codesMap.put("msoa", "S02001942");
+        codesMap.put("lau2", "S30000019");
+
+        Map<String, String> sortedCodes = new TreeMap<>(codesMap);
+        Map<String, String> responseMap = new TreeMap<>(postcodeDto.getCodes());
+        assertEquals(sortedCodes,responseMap);
+
+
+
+    }
 
 
 
